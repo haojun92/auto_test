38db7c2aba464dba9ee335db9415c91f93ed9417
haoyinyin <haoyinyin@mychery.com>
2026-01-16 17:26:58 +0800

    add diagnose capl

---
+++ CANoe15_Project/test.cs
@@ -0,0 +1,19 @@
+using System;
+using Vector.Tools;
+using Vector.CANoe.Runtime;
+using Vector.CANoe.Sockets;
+using Vector.CANoe.Threading;
+using Vector.Diagnostics;
+using Vector.Scripting.UI;
+using Vector.CANoe.TFS;
+using Vector.CANoe.VTS;
+
+
+
+public class test : TestModule
+{
+    public override void Main()
+    {
+    
+    }
+}
